319dc615c9ffc1cef64dcd099535d31cb00ad845
alreadyusedd <tema20029@gmail.com>
2023-04-05 17:40:52 +0300

    Рефактор

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,7 +7,6 @@ public class PlayerMovement : MonoBehaviour {
   public CharacterController2D controller;
 
   public float runSpeed = 40f;
-
   float horizontalMove = 0f;
   bool jump = false;
   bool crouch = false;
@@ -19,7 +18,7 @@ public class PlayerMovement : MonoBehaviour {
   [SerializeField] private LayerMask _groundLayer;
   [SerializeField] private Transform _groundCheck;
   public float jump1 = 5f;
-  // Update is called once per frame
+
   void Update () {
 
     horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
@@ -47,7 +46,6 @@ public class PlayerMovement : MonoBehaviour {
 
   void FixedUpdate ()
   {
-    // Move our character
     controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
     jump = false;
   }
